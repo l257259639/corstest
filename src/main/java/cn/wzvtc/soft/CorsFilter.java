22bac3a16ffbb7c982e5ddab091201b22fae9f67
l257259639 <43592022+l257259639@users.noreply.github.com>
2019-12-30 09:34:13 +0800

    Add files via upload

--- src/main/java/cn/wzvtc/soft/CorsFilter.java
+++ src/main/java/cn/wzvtc/soft/CorsFilter.java
@@ -1,29 +1,32 @@
 package cn.wzvtc.soft;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class CorsFilter extends OncePerRequestFilter {
+public class CorsFilter implements Filter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-        {
-            // CORS "pre-flight" request
-            //response.addHeader("Access-Control-Allow-Origin", "*");
-            //response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-            response.addHeader("Access-Control-Allow-Credentials","true");
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-            response.addHeader("Access-Control-Max-Age", "1800");//30 min
-        }
-        filterChain.doFilter(request, response);
+    final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CorsFilter.class);
+
+
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpServletRequest reqs = (HttpServletRequest) req;
+
+       // response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+      //  response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+      //  response.setHeader("Access-Control-Max-Age", "3600");
+     //   response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
     }
+    public void init(FilterConfig filterConfig) {}
+    public void destroy() {}
 }
--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,42 +1,54 @@
 package cn.wzvtc.soft;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-
-@CrossOrigin(origins = {"http://127.0.0.1:5500"})
-@RestController()
+@CrossOrigin("http://127.0.0.1:5500")
+@RestController("/" )
 public class TestController {
+    @Autowired
+    LvliRepository lvliRepository;
 
-
-    @RequestMapping(value = "/userinfo")
-    public Map<String, String> userinfo(HttpServletRequest httpServletRequest) {
-
-        Map<String, String> resultMap = new HashMap<>();
-        //判断是否已经登陆,判断SESSION里面是否有我的用户名信息
-        if (httpServletRequest.getSession(true).getAttribute("loginnumber") != null) {
-            resultMap.put("myname", (String) httpServletRequest.getSession().getAttribute("loginnumber"));
-            resultMap.put("mynumber", (String) httpServletRequest.getSession().getAttribute("username"));
+    @RequestMapping(value="/addlvli")
+    public void addlvli(String lvli){
+        Lvli lvli1=new Lvli("180",lvli);
+        this.lvliRepository.save(lvli1);
+    }
+    @RequestMapping(value ="/lvlilist")
+    public List<Lvli> getlvlist(){
+        return this.lvliRepository.findAll();
+    }
+    @RequestMapping(value="userinfo")
+    public Map<String,String> userinfo(HttpSession httpSession){
+        Map<String,String> resultMap=new HashMap<>();
+        if(httpSession.getAttribute("loginnumber")!=null){
+            resultMap.put("myname","董玲玲");
+            resultMap.put("mynumber","18002090209");
         }
+
         return resultMap;
     }
 
-    @RequestMapping(value = "/login")
-    public Map<String, String> login(@RequestBody Map<String, String> map, HttpServletRequest httpServletRequest) {
-        String password = map.get("password");
-        String number = map.get("number");
-        Map<String, String> resultMap = new HashMap<>();
-        if ("18002040132".equals(number) && "18002040132".equals(password)) {
-            //将用户名存入到session中
-            httpServletRequest.getSession().setAttribute("loginnumber", number);
-            httpServletRequest.getSession().setAttribute("username", number);
-            resultMap.put("result", "success");
+    @RequestMapping(value="login")
+    public Map<String,String> login(@RequestBody Map<String,String> map, HttpSession httpSession){
+        String password=map.get("password");
+        String number=map.get("number");
+        Map<String,String> resultMap=new HashMap<>();
+        if("18002090209".equals(number) && "18002090209".equals(password)){
+            httpSession.setAttribute("loginnumber",number);
+            resultMap.put("result","success");
         }
         return resultMap;
     }
-
-
+    @RequestMapping(value="logout")
+    public void login(HttpServletRequest httpServletRequest){
+        httpServletRequest.getSession().removeAttribute("loginnumber");
+    }
 }
