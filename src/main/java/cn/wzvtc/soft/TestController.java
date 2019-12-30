8a2f4868e19a6dcb6790ad69060987ae5f068fda
l257259639 <43592022+l257259639@users.noreply.github.com>
2019-12-30 09:31:30 +0800

    Add files via upload

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,22 +1,42 @@
 package cn.wzvtc.soft;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin
-@RestController("/" )
+
+@CrossOrigin(origins = {"http://127.0.0.1:5500"})
+@RestController()
 public class TestController {
 
-    @RequestMapping(value="/data.json",method={RequestMethod.GET})
-    public Map<String,String> bookById(){
-        Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","董玲玲");
-        resultMap.put("mynumber","18002090209");
+
+    @RequestMapping(value = "/userinfo")
+    public Map<String, String> userinfo(HttpServletRequest httpServletRequest) {
+
+        Map<String, String> resultMap = new HashMap<>();
+        //判断是否已经登陆,判断SESSION里面是否有我的用户名信息
+        if (httpServletRequest.getSession(true).getAttribute("loginnumber") != null) {
+            resultMap.put("myname", (String) httpServletRequest.getSession().getAttribute("loginnumber"));
+            resultMap.put("mynumber", (String) httpServletRequest.getSession().getAttribute("username"));
+        }
         return resultMap;
     }
+
+    @RequestMapping(value = "/login")
+    public Map<String, String> login(@RequestBody Map<String, String> map, HttpServletRequest httpServletRequest) {
+        String password = map.get("password");
+        String number = map.get("number");
+        Map<String, String> resultMap = new HashMap<>();
+        if ("18002040132".equals(number) && "18002040132".equals(password)) {
+            //将用户名存入到session中
+            httpServletRequest.getSession().setAttribute("loginnumber", number);
+            httpServletRequest.getSession().setAttribute("username", number);
+            resultMap.put("result", "success");
+        }
+        return resultMap;
+    }
+
+
 }
